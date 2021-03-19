fb8d8b537122e70619365c3e55a00ff62bbe0be6
Diego Saouda <diego@f360.com.br>
2021-03-19 15:04:24 -0300

    add termos

---
+++ layouts/default.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+      <Nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  font-family:
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+  background-color: #e8f1ff;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+.title {
+  color: #293856;
+  font-family: "Roboto Slab", Sans-serif;
+
+  display: block;
+  font-size: 2em;
+  margin-block-start: 0.67em;
+  margin-block-end: 0.67em;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+  font-weight: bold;
+}
+
+body {
+  font-size: 1.3em;
+}
+
+p {
+  margin-top: 20px;
+  margin-bottom: 20px;
+  letter-spacing: 1px;
+  line-height: 1.5em;
+
+  color: #7c88a5;
+}
+
+.container {
+  padding: 30px;
+}
+</style>
---
+++ pages/pessoas/termo-de-uso.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div style="background-color: hsl(321deg 69% 40%); padding: 40px;">
+      <Logo />
+    </div>
+    <div class="container">
+      <h1 class="title">Termo de uso</h1>
+      <p>
+        O presente Termo de Aceite define as condições gerais aplicáveis ao compartilhamento de dados
+        por <strong> P2CR SERVIÇOS DE INFORMÁTICA LTDA.</strong> (“CONTRATADA”) inscrita no CNPJ/ME sob o nº
+        18.519.837/0001-07, situada na Av. Marquês de São Vicente, 2.219, sala 1907, 19º andar, Vila
+        Água Branca, CEP 05036-040, São Paulo - SP, inscrita no CNPJ/MF sob o n.º 18.519.837/0001-
+        07, com seus parceiros comerciais (“PARCEIROS”) de acordo com as seguintes disposições:
+      </p>
+
+      <p>
+        1) Ao aceitar” você (“CONTRATANTE”) entende que seus dados de Vendas, Receitas, Gastos, Caixa,
+        Contas a pagar/receber, DRE e CNPJ serão compartilhados com nossos PARCEIROS, observando as
+        melhores técnicas disponíveis no momento do compartilhamento de dados para a
+        análise de disponibilização de ofertas de crédito através da plataforma F360 Finanças;
+      </p>
+
+      <p>
+        2) O aceite da CONTRATANTE nestes temos não oferece qualquer garantia do recebimento
+        de ofertas produtos ou serviços financeiros ou de crédito realizados pelos PARCEIROS da
+        CONTRATADA;
+      </p>
+
+      <p>
+        3) A CONTRATADA poderá compartilhar os dados e/ou informações cadastrais da
+        CONTRATANTE, de forma não anonimizada, para a possibilitar o fornecimento de ofertas
+        produtos ou serviços financeiros ou de crédito;
+      </p>
+
+      <p>
+        4) A CONTRATANTE poderá, a qualquer tempo, revogar o consentimento para o
+        compartilhamento de seus dados e/ou informações por meio do envio de e-mail para suporte@f360.com.br
+        solicitando a revogação do compartilhamento de dados com os parceiros;
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
